13e3862db9dafe4e4a3c6ea6265e4ec063f13a0e
Arun Chaudhary <arunchaudhary1811@gmail.com>
2024-09-14 18:20:09 +0530

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
                 </Link>
                 <Link to="/profile">
                 {currentUser ? (
-                    <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="Profile" />
+                    <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
                 ) : (
                     <li className="text-slate-700 hover:underline">Sign in</li>
                 )}
