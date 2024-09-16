c0ba866867da3b2791c045a4d516738f584f0b4f
Arun Chaudhary <arunchaudhary1811@gmail.com>
2024-09-17 05:26:54 +0530

    complete update user functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -59,7 +59,7 @@ export default function SignIn(){
                     onChange={handleChange}
                 />
                 <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-                    {loading ? "loading..." : "Sign In"}
+                    {loading ? "Loading..." : "Sign In"}
                 </button>
                 <OAuth />
             </form>
