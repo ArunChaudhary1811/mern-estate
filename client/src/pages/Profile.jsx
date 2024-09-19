7f19c9a9d1f509692fec64d1de74686eeea0e0a4
Arun Chaudhary <arunchaudhary1811@gmail.com>
2024-09-19 19:59:58 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
     signOutUserFailure 
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Profile(){
     const fileRef = useRef(null);
@@ -164,7 +165,11 @@ export default function Profile(){
 
                 <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
                     {loading ? "Loading..." : "Update"}
-                </button>
+                </button>,
+
+                <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+                    Create Listing
+                </Link>
             </form>
 
             <div className="flex justify-between mt-5">
