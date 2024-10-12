128e6850d348ffddef3162b773f22dfab56b1591
Arun Chaudhary <arunchaudhary1811@gmail.com>
2024-10-12 19:43:47 +0530

    create update listing api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -199,7 +199,7 @@ export default function Profile(){
 
                 <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
                     {loading ? "Loading..." : "Update"}
-                </button>,
+                </button>
 
                 <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
                     Create Listing
