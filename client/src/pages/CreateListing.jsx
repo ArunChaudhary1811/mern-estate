a539559ce4139ef934d1ed77877fef7cf99436e2
Arun Chaudhary <arunchaudhary1811@gmail.com>
2024-10-21 20:34:35 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -233,7 +233,9 @@ export default function CreateListing(){
                             />
                             <div className="flex flex-col items-center">
                                 <p>Regular Price</p>
-                                <span className="text-xs">($ / month)</span>
+                                {formData.type === "rent" && (
+                                    <span className="text-xs">($ / month)</span>
+                                )}
                             </div>
                         </div>
                         {formData.offer && (
@@ -250,7 +252,9 @@ export default function CreateListing(){
                                 />
                                 <div className="flex flex-col items-center">
                                     <p>Discounted Price</p>
-                                    <span className="text-xs">($ / month)</span>
+                                    {formData.type === "rent" && (
+                                        <span className="text-xs">($ / month)</span>
+                                    )}
                                 </div>
                             </div>
                         )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -248,7 +248,9 @@ export default function UpdateListing(){
                             />
                             <div className="flex flex-col items-center">
                                 <p>Regular Price</p>
-                                <span className="text-xs">($ / month)</span>
+                                {formData.type === "rent" && (
+                                    <span className="text-xs">($ / month)</span>
+                                )}
                             </div>
                         </div>
                         {formData.offer && (
@@ -265,7 +267,9 @@ export default function UpdateListing(){
                                 />
                                 <div className="flex flex-col items-center">
                                     <p>Discounted Price</p>
-                                    <span className="text-xs">($ / month)</span>
+                                    {formData.type === "rent" && (
+                                        <span className="text-xs">($ / month)</span>
+                                    )}
                                 </div>
                             </div>
                         )}
